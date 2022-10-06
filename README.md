ed25e0dab0d823e8137d2893e76b3d7fc2486981
Iancito pa <ianbarbera@gmail.com>
2022-10-05 22:48:49 -0300

    data de un planeta

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # infinite-scroll
+
+1) hacerse una cuenta en codewars.com y practicar algo
+2) me muestren en cartitas a todos los planetas de una pagina https://rickandmortyapi.com/api/location?page=1
---
+++ src/index.js
@@ -0,0 +1,35 @@
+// http://placekitten.com/200/200
+
+
+/*
+<div class="card">
+            <h1>card</h1>
+            <h2>sub</h2>
+            <img src="http://placekitten.com/200/200" alt="">
+            <div class="detail-container">
+                <h4>blabla</h4>
+                <h4>blable</h4>
+    </div>
+</div>
+*/
+
+const loadData = async () => {
+    const data = await fetch("https://rickandmortyapi.com/api/location?page=1")
+    const planetas = await data.json()
+    const primerPlaneta = planetas.results[0]
+
+    const container = document.getElementById("card-container")
+    console.log(container.innerHTML)
+    container.innerHTML = `<div class="card">
+    <h1>${primerPlaneta.name}</h1>
+    <h2>${primerPlaneta.dimension}</h2>
+    <img src="http://placekitten.com/200/200" alt="">
+    <div class="detail-container">
+        <h4>Type: ${primerPlaneta.type}</h4>
+        <h4>Population: ${primerPlaneta.residents.length    }</h4>
+</div>
+</div>`
+
+}
+
+loadData()
